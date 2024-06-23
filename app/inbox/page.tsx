e972ec871b7ec28b0a3fee8f8eef2b68eb6824ce
jemimacecil <121038719+jemimacecil@users.noreply.github.com>
2024-06-23 21:14:39 +0100

    play around

---
+++ app/inbox/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InboxPage = () => {
+    return (
+        <div>
+            <h1>Inbox</h1>
+        </div>
+    );
+}
+
+export default InboxPage;
---
+++ app/rented/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RentedPage = () => {
+    return (
+        <div>
+            <h1>Rented Tools</h1>
+        </div>
+    );
+}
+
+export default RentedPage;
