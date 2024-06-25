018b6ebd5fd8465f3c1bbad5753e79895acdf94e
sevtechcodes <sevimtas0101@gmail.com>
2024-06-25 13:46:09 +0200

    fixed for PR comments

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -7,27 +7,27 @@ import ToolCardComponent from '../components/ToolCard';
 const ToolsPage = () => {
 	const tools: ToolCard[] = [
 			{
-					_id: '1',
-					name: 'Hammer',
-					description: 'A tool used for driving nails',
-					location: 'Garage A',
-					dailyRate: 5,
-					weeklyRate: 30, // Add weeklyRate
-					monthlyRate: 100, // Add monthlyRate
-					picture: '',
-					liked: true,
-					available: true,
-					reviews: [
-							{
-									_id: 'review1',
-									authorId: 'user1',
-									content: 'This tool is very useful!',
-									createdAt: new Date('2023-06-20'),
-									toolCardId: '1',
-							}
-					],
-					ownerId: 'owner1',
-					toolCategoryId: 'category1',
+				_id: '1',
+				name: 'Hammer',
+				description: 'A tool used for driving nails',
+				location: 'Garage A',
+				dailyRate: 5,
+				weeklyRate: 30, 
+				monthlyRate: 100, 
+				picture: '',
+				liked: true,
+				available: true,
+				reviews: [
+					{
+						_id: 'review1',
+						authorId: 'user1',
+						content: 'This tool is very useful!',
+						createdAt: new Date('2023-06-20'),
+						toolCardId: '1',
+					}
+				],
+				ownerId: 'owner1',
+				toolCategoryId: 'category1',
 			},
 			{
 				_id: '1',
@@ -35,19 +35,19 @@ const ToolsPage = () => {
 				description: 'A tool used for driving nails',
 				location: 'Garage A',
 				dailyRate: 5,
-				weeklyRate: 30, // Add weeklyRate
-				monthlyRate: 100, // Add monthlyRate
+				weeklyRate: 30, 
+				monthlyRate: 100,
 				picture: '',
 				liked: true,
 				available: true,
 				reviews: [
-						{
-								_id: 'review1',
-								authorId: 'user1',
-								content: 'This tool is very useful!',
-								createdAt: new Date('2023-06-20'),
-								toolCardId: '1',
-						}
+					{
+						_id: 'review1',
+						authorId: 'user1',
+						content: 'This tool is very useful!',
+						createdAt: new Date('2023-06-20'),
+						toolCardId: '1',
+					}
 				],
 				ownerId: 'owner1',
 				toolCategoryId: 'category1',
@@ -62,8 +62,7 @@ const ToolsPage = () => {
 								{tools.map(tool => (
 										<div key={tool._id} className="tool-item"
 										>
-												<ToolCardComponent tool={tool} />
-										
+											<ToolCardComponent tool={tool} />
 										</div>
 								))}
 						</div>
