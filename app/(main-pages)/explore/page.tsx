6dbe2185dab98e09cd79db7c9654023334103a79
Brice <fromm.brice@gmail.com>
2024-06-26 12:01:32 +0200

    up to date version

--- app/(main-pages)/explore/page.tsx
+++ app/(main-pages)/explore/page.tsx
@@ -5,107 +5,109 @@ import { ToolCard, ToolsReviews } from '../../lib/types';
 import ToolCardComponent from '../../components/ToolCard';
 
 const ToolsPage = () => {
-	const tools: ToolCard[] = [
-			{
-				_id: '1',
-				name: 'Hammer',
-				description: 'A tool used for driving nails',
-				location: 'Garage A',
-				dailyRate: 5,
-				weeklyRate: 30, 
-				monthlyRate: 100, 
-				picture: '',
-				liked: true,
-				available: true,
-				reviews: [
-					{
-						_id: 'review1',
-						authorId: 'user1',
-						content: 'This tool is very useful!',
-						createdAt: new Date('2023-06-20'),
-						toolCardId: '1',
-					}
-				],
-				ownerId: 'owner1',
-				toolCategoryId: 'category1',
-			},
-			{
-				_id: '1',
-				name: 'Hammer',
-				description: 'A tool used for driving nails',
-				location: 'Garage A',
-				dailyRate: 5,
-				weeklyRate: 30, 
-				monthlyRate: 100,
-				picture: '',
-				liked: true,
-				available: true,
-				reviews: [
-					{
-						_id: 'review1',
-						authorId: 'user1',
-						content: 'This tool is very useful!',
-						createdAt: new Date('2023-06-20'),
-						toolCardId: '1',
-					}
-				],
-				ownerId: 'owner1',
-				toolCategoryId: 'category1',
-		},
-		{
-			_id: '1',
-			name: 'Hammer',
-			description: 'A tool used for driving nails',
-			location: 'Garage A',
-			dailyRate: 5,
-			weeklyRate: 30, 
-			monthlyRate: 100, 
-			picture: '',
-			liked: true,
-			available: true,
-			reviews: [
-				{
-					_id: 'review1',
-					authorId: 'user1',
-					content: 'This tool is very useful!',
-					createdAt: new Date('2023-06-20'),
-					toolCardId: '1',
-				}
-			],
-			ownerId: 'owner1',
-			toolCategoryId: 'category1',
-		},
-		{
-			_id: '1',
-			name: 'Hammer',
-			description: 'A tool used for driving nails',
-			location: 'Garage A',
-			dailyRate: 5,
-			weeklyRate: 30, 
-			monthlyRate: 100,
-			picture: '',
-			liked: true,
-			available: true,
-			reviews: [
-				{
-					_id: 'review1',
-					authorId: 'user1',
-					content: 'This tool is very useful!',
-					createdAt: new Date('2023-06-20'),
-					toolCardId: '1',
-				}
-			],
-			ownerId: 'owner1',
-			toolCategoryId: 'category1',
-	}
-	];
-	return (
-<div className="container mx-auto px-2 py-2">
-      <h1 className="text-2xl font-bold mb-4 text-center">Discover Your Ideal Tool Here!</h1>
+  const tools: ToolCard[] = [
+    {
+      _id: '1',
+      name: 'Hammer',
+      description: 'A tool used for driving nails',
+      location: 'Garage A',
+      dailyRate: 5,
+      weeklyRate: 30,
+      monthlyRate: 100,
+      picture: '',
+      liked: true,
+      available: true,
+      reviews: [
+        {
+          _id: 'review1',
+          authorId: 'user1',
+          content: 'This tool is very useful!',
+          createdAt: new Date('2023-06-20'),
+          toolCardId: '1',
+        },
+      ],
+      ownerId: 'owner1',
+      toolCategoryId: 'category1',
+    },
+    {
+      _id: '1',
+      name: 'Hammer',
+      description: 'A tool used for driving nails',
+      location: 'Garage A',
+      dailyRate: 5,
+      weeklyRate: 30,
+      monthlyRate: 100,
+      picture: '',
+      liked: true,
+      available: true,
+      reviews: [
+        {
+          _id: 'review1',
+          authorId: 'user1',
+          content: 'This tool is very useful!',
+          createdAt: new Date('2023-06-20'),
+          toolCardId: '1',
+        },
+      ],
+      ownerId: 'owner1',
+      toolCategoryId: 'category1',
+    },
+    {
+      _id: '1',
+      name: 'Hammer',
+      description: 'A tool used for driving nails',
+      location: 'Garage A',
+      dailyRate: 5,
+      weeklyRate: 30,
+      monthlyRate: 100,
+      picture: '',
+      liked: true,
+      available: true,
+      reviews: [
+        {
+          _id: 'review1',
+          authorId: 'user1',
+          content: 'This tool is very useful!',
+          createdAt: new Date('2023-06-20'),
+          toolCardId: '1',
+        },
+      ],
+      ownerId: 'owner1',
+      toolCategoryId: 'category1',
+    },
+    {
+      _id: '1',
+      name: 'Hammer',
+      description: 'A tool used for driving nails',
+      location: 'Garage A',
+      dailyRate: 5,
+      weeklyRate: 30,
+      monthlyRate: 100,
+      picture: '',
+      liked: true,
+      available: true,
+      reviews: [
+        {
+          _id: 'review1',
+          authorId: 'user1',
+          content: 'This tool is very useful!',
+          createdAt: new Date('2023-06-20'),
+          toolCardId: '1',
+        },
+      ],
+      ownerId: 'owner1',
+      toolCategoryId: 'category1',
+    },
+  ];
+  return (
+    <div className='container mx-auto px-2 py-2'>
+      <h1 className='text-2xl font-bold mb-4 text-center'>
+        Discover Your Ideal Tool Here!
+      </h1>
       <Link href={`/tools`}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {tools.map(tool => (
-            <div key={tool._id} className="tool-item">
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {tools.map((tool) => (
+            <div key={tool._id} className='tool-item'>
               <ToolCardComponent tool={tool} />
             </div>
           ))}
@@ -113,5 +115,5 @@ const ToolsPage = () => {
       </Link>
     </div>
   );
-}
+};
 export default ToolsPage;
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,8 +10,6 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-
-
 const NavBar = () => {
   const currentPath = usePathname();
 
