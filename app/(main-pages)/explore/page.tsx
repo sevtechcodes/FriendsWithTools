e2ac21a10225e0338840674af775f5bbd30ed2f7
Brice <fromm.brice@gmail.com>
2024-06-26 21:01:37 +0200

    updated version

--- app/(main-pages)/explore/page.tsx
+++ app/(main-pages)/explore/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React from 'react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
@@ -34,114 +34,16 @@ const ToolsPage = () => {
       <h1 className='text-2xl font-bold mb-4 text-center'>
         Discover Your Ideal Tool Here!
       </h1>
-      <Link href={'/tools'}>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-          {tools.map((tool) => (
-            <Link href={`/tools/${tool.id}`}>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {tools.map((tool) => (
+          <Link href={`/tools/${tool.id}`}>
             <div key={tool.id} className='tool-item'>
               <ToolCardComponent tool={tool} />
             </div>
-            </Link>
-          ))}
-        </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
 export default ToolsPage;
-
-
-
-// const tools: ToolCard[] = [
-//   {
-//     _id: '1',
-//     name: 'Hammer',
-//     description: 'A tool used for driving nails',
-//     location: 'Garage A',
-//     dailyRate: 5,
-//     weeklyRate: 30,
-//     monthlyRate: 100,
-//     picture: '',
-//     liked: true,
-//     available: true,
-//     reviews: [
-//       {
-//         _id: 'review1',
-//         authorId: 'user1',
-//         content: 'This tool is very useful!',
-//         createdAt: new Date('2023-06-20'),
-//         toolCardId: '1',
-//       },
-//     ],
-//     ownerId: 'owner1',
-//     toolCategoryId: 'category1',
-//   },
-//   {
-//     _id: '1',
-//     name: 'Hammer',
-//     description: 'A tool used for driving nails',
-//     location: 'Garage A',
-//     dailyRate: 5,
-//     weeklyRate: 30,
-//     monthlyRate: 100,
-//     picture: '',
-//     liked: true,
-//     available: true,
-//     reviews: [
-//       {
-//         _id: 'review1',
-//         authorId: 'user1',
-//         content: 'This tool is very useful!',
-//         createdAt: new Date('2023-06-20'),
-//         toolCardId: '1',
-//       },
-//     ],
-//     ownerId: 'owner1',
-//     toolCategoryId: 'category1',
-//   },
-//   {
-//     _id: '1',
-//     name: 'Hammer',
-//     description: 'A tool used for driving nails',
-//     location: 'Garage A',
-//     dailyRate: 5,
-//     weeklyRate: 30,
-//     monthlyRate: 100,
-//     picture: '',
-//     liked: true,
-//     available: true,
-//     reviews: [
-//       {
-//         _id: 'review1',
-//         authorId: 'user1',
-//         content: 'This tool is very useful!',
-//         createdAt: new Date('2023-06-20'),
-//         toolCardId: '1',
-//       },
-//     ],
-//     ownerId: 'owner1',
-//     toolCategoryId: 'category1',
-//   },
-//   {
-//     _id: '1',
-//     name: 'Hammer',
-//     description: 'A tool used for driving nails',
-//     location: 'Garage A',
-//     dailyRate: 5,
-//     weeklyRate: 30,
-//     monthlyRate: 100,
-//     picture: '',
-//     liked: true,
-//     available: true,
-//     reviews: [
-//       {
-//         _id: 'review1',
-//         authorId: 'user1',
-//         content: 'This tool is very useful!',
-//         createdAt: new Date('2023-06-20'),
-//         toolCardId: '1',
-//       },
-//     ],
-//     ownerId: 'owner1',
-//     toolCategoryId: 'category1',
-//   },
-// ];
