697c6d5cef206ea007d57ec63ad6a75c4b11dfff
Brice <fromm.brice@gmail.com>
2024-06-27 21:30:31 +0200

    styling in progress

--- app/(no-nav)/form/page.tsx
+++ app/(no-nav)/form/page.tsx
@@ -146,8 +146,12 @@ const Form = () => {
   return (
     <>
       <header className='border-grey h-20 shadow-md mb-5 bg-slate-200 '>
-        <Link href='/rented' className='h-15 w-15'>
-          <ChevronLeftIcon className='h-12 w-12 border pl-5' />
+        <Link href='/rented'>
+          {/* <ChevronLeftIcon className='h-15 w-11  border ml-5 pt-5'/> */}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-12 ml-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+
         </Link>
       </header>
       <div className='flex justify-center items-center  h-200'>
