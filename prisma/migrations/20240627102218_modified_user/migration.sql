9e0207ee4570f758ea760325aa34c7383a685a84
wyna <queralt.guillen@gmail.com>
2024-06-27 12:32:22 +0200

    re-generated prisma  migrations

--- prisma/migrations/20240627102218_modified_user/migration.sql
+++ prisma/migrations/20240627102218_modified_user/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE "User" (
     "lastName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "profilePicture" TEXT NOT NULL,
+    "profilePicture" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -26,6 +26,22 @@ CREATE TABLE "ToolCard" (
     "toolCategoryId" TEXT NOT NULL,
 
     CONSTRAINT "ToolCard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WishList" (
+    "id" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "WishList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ToolsOnWishlists" (
+    "toolId" TEXT NOT NULL,
+    "wishlistId" TEXT NOT NULL,
+
+    CONSTRAINT "ToolsOnWishlists_pkey" PRIMARY KEY ("toolId","wishlistId")
 );
 
 -- CreateTable
@@ -66,11 +82,23 @@ CREATE TABLE "Message" (
     CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "WishList_ownerId_key" ON "WishList"("ownerId");
+
 -- AddForeignKey
 ALTER TABLE "ToolCard" ADD CONSTRAINT "ToolCard_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ToolCard" ADD CONSTRAINT "ToolCard_toolCategoryId_fkey" FOREIGN KEY ("toolCategoryId") REFERENCES "ToolCategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WishList" ADD CONSTRAINT "WishList_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ToolsOnWishlists" ADD CONSTRAINT "ToolsOnWishlists_toolId_fkey" FOREIGN KEY ("toolId") REFERENCES "ToolCard"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ToolsOnWishlists" ADD CONSTRAINT "ToolsOnWishlists_wishlistId_fkey" FOREIGN KEY ("wishlistId") REFERENCES "WishList"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ToolsReviews" ADD CONSTRAINT "ToolsReviews_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
