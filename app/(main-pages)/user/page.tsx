6bec04e1675c037532b3326fe1637ba3d4ae014e
wyna <queralt.guillen@gmail.com>
2024-06-27 11:44:51 +0200

    added logout button

--- app/(main-pages)/user/page.tsx
+++ app/(main-pages)/user/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import NavBar from '../../components/NavBar';
+import { Button } from "@/components/ui/button"
 import { Avatar, AvatarImage, AvatarFallback } from '@radix-ui/react-avatar';
 import UserTabs from '@/app/components/UserTabs';
 
 const UserPage = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-center'>
       <header className=' flex row-span-1 row py-4 px-8  border rounded border-solid border-zinc-400  bg-darkGreen items-center justify-start mb-1'>
         <Avatar className='h-14 w-14 mr-8'>
           <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" className='rounded-full' />
@@ -16,7 +16,8 @@ const UserPage = () => {
           <p className='text-xs text-slate-50'>Created on June 25th, 2024</p>
         </div>
       </header>
-      <UserTabs/>
+      <UserTabs />
+      <Button variant='destructive' className='flex justify-self-center w-52 ml-[5.5rem] my-3'>Log out</Button>
     </div>
   );
 };
