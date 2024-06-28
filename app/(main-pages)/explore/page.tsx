85f3549b9e022dd605c483322ca7c95c3d254a3a
wyna <queralt.guillen@gmail.com>
2024-06-28 18:52:55 +0200

    fixed scroll, toolCard not under navBar

--- app/(main-pages)/explore/page.tsx
+++ app/(main-pages)/explore/page.tsx
@@ -34,7 +34,7 @@ const ToolsPage = () => {
       <h1 className='text-2xl font-bold mb-4 text-center'>
         Discover Your Ideal Tool Here!
       </h1>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-scroll mb-16'>
         {tools.map((tool) => (
           <div key={tool.id} className='tool-item'>
             <ToolCardComponent tool={tool} />
