47da35c4c5970fa714f2471af5ade13ec0d4aa89
wyna <queralt.guillen@gmail.com>
2024-06-28 10:58:55 +0200

    post to wishlist working - hardcoded ownerId

--- app/api/wishlist/route.ts
+++ app/api/wishlist/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../../prisma/db'; 
+import { WishList } from '../../lib/types';
 
 export async function GET (request: NextRequest) {
   try {
@@ -7,6 +8,27 @@ export async function GET (request: NextRequest) {
     return NextResponse.json(tools);
   } catch (error) {
     return NextResponse.json({ error: 'Failed to fetch favorited tools' }, { status: 500 });
+  }
+}
+
+export async function POST (request: NextRequest) { //?
+  let favedTool = await request.json();
+  console.log('before try favedTool', favedTool);
+  console.log('favedTool id', favedTool.id);
+  try {
+    const wishlist: Partial<WishList> = await prisma.wishList.create({
+      data: {
+        ownerId: '85d0c4fe-439c-4adb-9343-1b8fdcb7c9e4',
+        list: {
+          connect: [{id: favedTool.id}]
+        }
+      },
+    });
+    console.log('favedTool', favedTool);
+    return NextResponse.json(wishlist);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: `${error}` }, { status: 500 });
   }
 }
 
