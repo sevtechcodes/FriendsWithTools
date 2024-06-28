6c7e816c51621b9be391d0b2c8b78300dc9a8ddd
sevtechcodes <sevimtas0101@gmail.com>
2024-06-28 18:24:28 +0200

    started building the login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Login from './(auth)/login/page';
 import SearchBar from './components/SearchBar';
 
 export default function Home () {
   return (
     <main>
       <div>
+			<h1>Explore Page</h1>
+			<Login></Login>
         
       </div>
       <h1>Explore Page</h1>
