8f696634516cf7d0fc404df236dfba6fc7bd8bc7
sevtechcodes <sevimtas0101@gmail.com>
2024-07-01 15:25:01 +0200

    authantication added

--- app/(no-nav)/form/page.tsx
+++ app/(no-nav)/form/page.tsx
@@ -24,7 +24,7 @@ const Form = () => {
     picture: '', // Added for picture state
     liked: false,
     available: true,
-    ownerId: 'acaa5f19-db41-44b2-a6a9-a32e051575b8', // Replace with the actual user ID
+    ownerId: '3a7a540a-c85f-4b20-ae10-59cd179fc482', // Replace with the actual user ID
     id: uuidv4(),
     reviews: [],
     toolCategoryId: '',
@@ -126,7 +126,7 @@ const Form = () => {
         monthlyRate: 0,
         liked: false,
         available: true,
-        ownerId: '438088e9-19de-45f9-8eec-9ebbce2c0678', // Replace with the actual 
+        ownerId: '3a7a540a-c85f-4b20-ae10-59cd179fc482', // Replace with the actual 
         id: uuidv4(),
         reviews: [],
         toolCategoryId: '',
